7a006ac0b96f3e36122ed5f763124437a089a398
TiwariAry <135610893+TiwariAry@users.noreply.github.com>
2025-06-17 00:06:58 +0530

    Major change
    Add Autocompletion, Posters, and Description

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+
+import pandas as pd
 
 from recommender import recommender_function
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+
+
+# API routes
+
+# API route for fetching Movie list
+@app.route('/api/movielist')
+def getMovies():
+	return jsonify(pd.read_csv('movie_names.csv')['original_title'].dropna().tolist())
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -17,8 +27,8 @@ def home_page():
 @app.route('/results/<string:movie>')
 # Expects one argument as specified by the route
 def results(movie):
-	movie_list = recommender_function(movie)
-	return render_template('results.html', movies=movie_list)
+	movie_info_list = recommender_function(movie)
+	return render_template('results.html', movies=movie_info_list)
 
 
 if __name__ == "__main__":
